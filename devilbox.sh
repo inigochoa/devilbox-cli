f999c1275f35244ac982b4858362a69e7552b72c
Inigo <inigochoa@gmail.com>
2019-07-08 21:22:56 +0200

    Devilbox command: added sources

---
+++ devilbox.sh
@@ -0,0 +1,11 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# Configuration.
+source "$DIR/config/codes.sh"
+source "$DIR/config/colors.sh"
+source "$DIR/config/config.sh"
+
+# Utils.
+source "$DIR/utils/codes.sh"
+source "$DIR/utils/messages.sh"
+source "$DIR/utils/prompt.sh"
