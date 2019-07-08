7e614812e224f47d00d4f729042459f8507ee728
Inigo <inigochoa@gmail.com>
2019-07-08 21:13:09 +0200

    Config: color codes

---
+++ config/colors.sh
@@ -0,0 +1,11 @@
+# Message colors.
+
+COLOR_DEFAULT=$(tput sgr0)
+COLOR_RED=$(tput setaf 1)
+COLOR_GREEN=$(tput setaf 2)
+COLOR_YELLOW=$(tput setaf 3)
+COLOR_BLUE=$(tput setaf 4)
+COLOR_PURPLE=$(tput setaf 5)
+COLOR_CYAN=$(tput setaf 6)
+COLOR_LIGHT_GRAY=$(tput setaf 7)
+COLOR_DARK_GRAY=$(tput setaf 0)
