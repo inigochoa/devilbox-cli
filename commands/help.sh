07c31dff56b4a65e2fb4cf6826eeedc6e5b55c17
Inigo <inigochoa@gmail.com>
2019-07-09 16:50:20 +0200

    Commands (help): add help info for status command

--- commands/help.sh
+++ commands/help.sh
@@ -6,6 +6,7 @@ __devilboxHelp() {
     __devilboxHelpOption "devilbox [--folder|-f]" "${COLOR_YELLOW}Move${COLOR_DEFAULT} to the devilbox folder."
     __devilboxHelpOption "devilbox [--help|-h]" "Display devilbox CLI command ${COLOR_YELLOW}help${COLOR_DEFAULT}."
     __devilboxHelpOption "devilbox [--open|-o]" "Open devilbox ${COLOR_YELLOW}intranet${COLOR_DEFAULT} in browser."
+    __devilboxHelpOption "devilbox [--status|-s]" "Show devilbox docker container ${COLOR_YELLOW}status${COLOR_DEFAULT}."
     __devilboxHelpOption "devilbox [--www|-w]" "Move current directory to devilbox ${COLOR_YELLOW}www folder${COLOR_DEFAULT}."
 }
 
