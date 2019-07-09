fae9c40cbea0329b024f399c96821baad239e2ba
Inigo <inigochoa@gmail.com>
2019-07-09 17:25:00 +0200

    Commands (docker): fix status permission error

--- commands/docker.sh
+++ commands/docker.sh
@@ -56,7 +56,7 @@ __devilboxUpAll() {
 # Check if devilbox is running.
 __isRunning() {
     local all
-    all=$(docker-compose ps 2> /dev/null | grep "devilbox" | awk '{print $3}' | grep "Up")
+    all=$(sudo docker-compose ps 2> /dev/null | grep "devilbox" | awk '{print $3}' | grep "Up")
 
     if __was_success; then
         return "$OK_CODE";
