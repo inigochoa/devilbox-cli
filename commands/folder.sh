caae6724f30ae14e046742637a976e1af49ed986
Inigo <inigochoa@gmail.com>
2019-07-09 16:25:04 +0200

    Commands: check if variables are present

--- commands/folder.sh
+++ commands/folder.sh
@@ -1,9 +1,21 @@
 # Move to devilbox folder.
 __devilboxFolder() {
+    if [ -z "$DEVILBOX_PATH" ]; then
+        __error "Variable DEVILBOX_PATH not found"
+
+        return "$KO_CODE"
+    fi
+
     cd $DEVILBOX_PATH
 }
 
 # Move to devilbox project folder.
 __devilboxProjectsFolder() {
+    if [ -z "$DEVILBOX_PROJECTS" ]; then
+        __error "Variable DEVILBOX_PROJECTS not found"
+
+        return "$KO_CODE"
+    fi
+
     cd $DEVILBOX_PROJECTS
 }
